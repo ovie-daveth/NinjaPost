f9d4600b6361638088ea5111119fef9273c579f2
ovie-davet <oviedavid77@mail.com>
2023-03-07 15:13:43 +0100

    Added routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,20 @@
 import React from 'react'
+import {Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
+import CreatePost from './pages/CreatePost'
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 const App = () => {
   return (
-    <div className=' text-center text-blue-400'>
-      Welcome
-    </div>
+    <>
+    <Header />
+    <Routes>
+    <Route path='/*' element={<Home />} />
+    <Route path="create" element={<CreatePost />} />
+  </Routes>
+  <Footer />
+  </>
   )
 }
 
